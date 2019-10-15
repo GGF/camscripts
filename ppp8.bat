c180716f1ba38e1dbce3f21cc9445f8e55b37621
ggf <georgeg.fedoroff@gmail.com>
2019-10-15 09:25:35 +0300

    drill scale

--- ppp8.bat
+++ ppp8.bat
@@ -1,2 +1,3 @@
 @rem echo off
-pcphoto %1 -s 1 -o N  -c x:\ps_emma\pcphoto\pcad85\my.ppp -a x:\ps_emma\pcphoto\pcad85\gerber.apr
+rem pcphoto %1 -s 1 -o N -c x:\ps_emma\pcphoto\pcad85\my.ppp -a x:\ps_emma\pcphoto\pcad85\gerber.apr
+pcphoto %1 -s 1 -o N -c x:\ps_emma\pcphoto\pcad85\my.ppp -a x:\ps_emma\pcphoto\pcad85\grb-inch.APR
