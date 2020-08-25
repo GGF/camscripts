21d083deb2fee611ca1a9018c77105f60fbcf34a
Igor Fedoroff <georgeg.fedoroff@gmail.com>
2020-08-25 21:22:15 +0300

    Script for change directory lastwrite time by his files

---
+++ setDirTimeByFileTime.ps1
@@ -0,0 +1,16 @@
+﻿#
+# Скрипт для изменения дат изменения каталогов по дате последнего файла в нем
+#
+
+# получим все каталоги ниже текущего
+$dirs = Get-ChildItem -Directory -Recurse
+# для каждого из них
+foreach ($dir in $dirs) {
+   # соберем все только файлы сортированные по времени последнего изменения в нисходящем порядке
+   $files = Get-ChildItem -Path $dir.FullName -File | Sort-Object -Property LastWriteTime -Descending
+   # если такие файлы нашлись (каталоги бывают пустые)
+   if ($files.Length) {
+        #установим дату этого каталога по первому в списке, то есть из-за сортировки самому новому
+        $dir.LastWriteTime = $files[0].LastWriteTime
+   }
+}
