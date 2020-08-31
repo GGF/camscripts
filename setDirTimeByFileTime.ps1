48d22b325d1a228b88235cab7670cde1a929bb4c
Фёдоров Игорь Юрьевич <i.fedorov@impuls.local>
2020-08-31 13:35:26 +0300

    add set dir time

---
+++ setDirTimeByFileTime.ps1
@@ -0,0 +1,12 @@
+﻿# получим все каталоги ниже текущего
+$dirs = Get-ChildItem -Directory -Recurse
+# для каждого из них
+foreach ($dir in $dirs) {
+   # соберем все только файлы сортированные по времени последнего изменения в нисходящем порядке
+   $files = Get-ChildItem -Path $dir.FullName -File | Sort-Object -Property LastWriteTime -Descending
+   # если такие файлы нашлись (каталоги бывают пустые)
+   if ($files.Length) {
+        #установим дату этого каталога по первому в списке, то есть из-за сортировки самому новому
+        $dir.LastWriteTime = $files[0].LastWriteTime
+   }
+}
